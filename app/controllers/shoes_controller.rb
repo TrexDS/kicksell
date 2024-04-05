20f222ede0581e09bfc79a25ab630dd490048433
Dan Nguyen <sydan05071997@gmail.com>
2024-04-05 20:19:58 +1030

    edit and update actions did not work on shoe model

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,10 +1,12 @@
 class ShoesController < ApplicationController
+  before_action :shoe_list, only: [:show, :edit]
   def index
+    # raise
     @shoes = Shoe.all
   end
 
   def show
-    @shoe = Shoe.find(params[:id])
+    # @shoe = Shoe.find(params[:id])
     @user = User.where(id: @shoe.user_id)
     # raise
   end
@@ -24,11 +26,27 @@ class ShoesController < ApplicationController
     else
       render :new
     end
+
+    def edit
+      raise
+    end
+
+    def update
+      raise
+      @shoe = Shoe.find(params[:id])
+      @shoe.update(shoe_params)
+      redirect_to shoe_path(@shoe)
+    end
+
   end
 
   private
 
   def shoe_params
     params.require(:shoe).permit(:name, :description, :price)
+  end
+
+  def shoe_list
+    @shoe = Shoe.find(params[:id])
   end
 end
